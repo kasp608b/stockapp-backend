8afcbc4aa89ddbb8d970a690d69e7599c74520e6
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-03-20 21:48:47 +0100

    Added mock data

--- src/stock/core/services/stock/stock.service.ts
+++ src/stock/core/services/stock/stock.service.ts
@@ -4,7 +4,38 @@ import { IStockService } from '../../primary-ports/stock.service.interface';
 
 @Injectable()
 export class StockService implements IStockService {
-  stocks: Stock[];
+  stocks: Stock[] = [
+    {
+      id: 'Vystas',
+      price: 1188.0,
+      init_price: 1188.0,
+      desc: 'Great danish company that produces Windmills',
+    },
+    {
+      id: 'Uniti',
+      price: 24000.22,
+      init_price: 24000.22,
+      desc: 'A real-time engine',
+    },
+    {
+      id: 'Mursk',
+      price: 25000.21,
+      init_price: 25000.21,
+      desc: 'A shipping company',
+    },
+    {
+      id: 'Dinske Bonk',
+      price: 113.92,
+      init_price: 113.92,
+      desc: 'A danish bank',
+    },
+    {
+      id: 'Nova Nordysk',
+      price: 450.02,
+      init_price: 450.02,
+      desc: 'A danish pharmaceutical company',
+    },
+  ];
   getStocks(): Stock[] {
     return this.stocks;
   }
