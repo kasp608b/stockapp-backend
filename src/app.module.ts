09bbe399b13a287efe13b75f1621e1e8d97f5c4f
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-03-21 18:07:03 +0100

    begining database stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { StockModule } from './stock/api/stock.module';
 import { StockService } from './stock/core/services/stock/stock.service';
-
+import * as Joi from '@hapi/joi';
 @Module({
   imports: [StockModule],
   controllers: [],
